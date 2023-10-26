a786ba15e92d08cbef96ba30faa0a2be658e4380
chandan shutradhar <145096947+chandandev72@users.noreply.github.com>
2023-10-26 14:07:18 +0600

    y

---
+++ array.js
@@ -0,0 +1,156 @@
+var mixed = ['Ali','Rupangkar',24,35,56,123,166,177];
+var number = [13,14,16,19,39,366,245,257,589];
+var index =number.indexOf(245);
+// console.log(index);
+// // console.log(number.length)
+
+var number = [13,14,16,19,39,366,245,257,589];
+number[3] = 322;
+// console.log(number)
+
+var students = ['Mazbah', 'Rupangkar', 'al-amin', 'candon'];
+students.push('Dipu');
+students.push('Ripon');
+students.push('sipon');
+students.pop('dipon');
+// console.log(students)
+
+
+// var firstPush = ['jamal','kamal','forid'];
+// firstPush.unshift('Rupock')
+// console.log(firstPush);
+
+var firstPop = ['Ripon','shipon','Dipon','kakon'];
+firstPop.shift();
+// console.log(firstPop);
+
+// ami < tumi
+// ami > tumi
+// ami <= tumi
+
+// ami >= tumi
+// ami == tumi
+// ami != tumi
+
+var dip = 99 ;
+var sabonti = 100 ;
+if(dip > sabonti) {
+    // console.log('dip good boy');
+}
+
+// var dip = 99 ;
+// var sabonti = 100 ;
+// if("dip < sabonti"){
+//     console.log("sabonti good girl");
+// }
+
+var dip = 99 ;
+var sabonti = 100 ;
+if(dip >= sabonti) {
+    // console.log('dip good boy');
+}
+var dip = 99 ;
+var sabonti = 100 ;
+if(dip <= sabonti) {
+    // console.log('same noy');
+}
+
+var hasFood = true ;
+// if food theke
+
+// food daw
+// jodi na thake dio na
+if (hasFood == true) 
+   
+    // if food theke
+    
+    // food daw
+    // jodi na thake dio na
+    if (hasFood == true) {
+    //     console.log("khabar daw");
+    // }else {
+    //     console.log("khabar nai re beta");
+    }
+
+var hasFood = true ;
+if (hasFood == false ) {
+    console.log("khabar daw");
+}else {
+    // console.log('na thakle dio na');
+}
+
+var smallBrother = 13 ;
+if (smallBrother > 15){
+    // console.log('you are allow to show jomila sundori');
+
+}else{
+    // console.log('not allow')
+}
+
+// var hasFlat = true ;
+// var govtJob = false ;
+// var money = 200000 ;
+
+// if (hasFlat == true && govtJob == true){
+//     console.log('Ajke tomar biye hobe');
+// } else {
+//     console.log('tomar kopale biye nai re vai');
+// }
+
+// var hasFlat = true ;
+// var govtJob = false ;
+// var money = 200000 ;
+
+// if (hasFlat == true || govtJob == true){
+//     console.log('Ajke tomar biye hobe');
+// } else {
+//     console.log('tomar kopale biye nai re vai');
+// }
+
+// var hasFlat = true ;
+// var govtJob = false ;
+// var money = 200000 ;
+
+// if (hasFlat == true &&  money >= 200000 || govtJob == true ){
+// //     console.log('Ajke tomar biye hobe');
+// // } else {
+// //     console.log('tomar kopale biye nai re vai');
+// // }
+
+// var hasFlat = true ;
+// var govtJob = false ;
+// var money = 200000 ;
+
+// if (hasFlat == true && govtJob == false){
+//     console.log('Ajke tomar biye hobe');
+// } else {
+//     console.log('tomar kopale biye nai re vai');
+// }
+
+// var myTaka = 50;
+// var tea = 10;
+// var denis = 30;
+
+
+  
+// if (myTaka > denis) {
+   
+//         console.log('danis diye tea khabo re vai');
+    
+// }else if (myTaka > tea){
+//     console.log('tea khabo re vai');
+// }
+
+var myTaka = 50;
+var tea = 10;
+var denis = 60;
+var wellPacked = true;
+
+  
+if (myTaka > denis) {
+   if(wellPacked > true) {
+        console.log('danis diye tea khabo re vai');
+   }
+}else if (myTaka > tea){
+    console.log('tea khabo re vai');
+}
---
+++ index.js
@@ -0,0 +1,97 @@
+var number = 200;
+// console.log(typeof(number))
+// console.log(number)
+
+var text = "Rupangkar"
+// console.log(typeof(text));
+// console.log(text);
+
+var isWeatherSunny = false;
+// console.log(typeof(isWeatherSunny));
+// console.log(isWeatherSunny)
+
+// var isEshikhonOpen = true;
+// console.log(isEshikhonOpen)
+// var first = 'Rupangkar';
+// var last = 'majumder';
+// var total = first +" "+ last;
+// console.log(total)
+
+var price1 = 99;
+var price2 = 93;
+var total = price1 + price2;
+// console.log(total);
+
+var price = 10 +20;
+// console.log(price);
+
+var eggPrice = 15;
+var onionPrice = 100;
+// var markatingPrice = eggPrice + onionPrice;
+// console.log(markatingPrice)
+
+var grilFriend = false;
+var PIE =3.1416;
+// console.log(PIE ,grilFriend)
+
+var bataShoe =99;
+var shock = 50 ;
+var total = bataShoe + shock;
+// console.log(total)
+
+var chicken = "99.9999"
+var convert =parseFloat(chicken)
+var suger = 100;
+var suparMarket = convert + suger;
+// console.log(suparMarket)
+
+// var chicken = "99.999";
+// var convert = parseInt(chicken);
+// console.log( chicken , convert);
+
+var amount = 500;
+var people = 10;
+
+var division = amount / people;
+var minus = amount - people;
+var multi = amount * people ;
+var samation = amount + people ;
+// console.log(samation);
+
+// var mayName = "Majumder Rupangkar"
+// mayName = "Rupangkar Majumder"
+// console.log(mayName);
+
+var number = "000 555";
+number = "555 000"
+// console.log(number); 
+
+var price = 93;
+// price = 93 + 2;
+price += 2;
+// console.log(price);
+
+var product = 100;
+product += 20;
+// console.log(product)
+
+var age = 12;
+// age = 12 + 1;
+age +=1
+age++
+// console.log(age)
+// var age = 35;
+// age -= 3;
+// age--
+// console.log(age)
+
+var price = 99;
+// console.log(typeof(price))
+
+var today = new Date();
+// console.log(today);
+ 
+var firstNumbar = 0.1;
+var lastNumbar = 0.2;
+var total = firstNumbar + lastNumbar ;
+console.log(total)
